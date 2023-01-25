8c409459fc2f86a1714e291d9640e7f36ebe3a89
github-mariyaweb <96mariyask@gmail.com>
2023-01-25 09:59:03 +0300

    init: start task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reverseRes = "";
+    let numStr = Math.abs(n).toString();
+
+    for (let i = numStr.length; i >= 0; i--) {
+        if ((numStr.length - 1) == 0) {
+            reverseRes += "";
+        } else {
+            reverseRes += numStr.charAt(i);
+        }
+    }
+    return reverseRes;
 }
